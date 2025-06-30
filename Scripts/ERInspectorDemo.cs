f7ffd39bf36a498cb3a789b3ed3a0b4df8a250ef
Eden Rompas <edenrompaskanter@gmail.com>
2025-07-01 03:06:48 +0800

    Initial commit
    Add initial project files and setup.

---
+++ Scripts/ERInspectorDemo.cs
@@ -0,0 +1,42 @@
+using EREditor.Inspector;
+using UnityEngine;
+
+public class ERInspectorDemo : MonoBehaviour
+{
+    [Title("Object")]
+    [SerializeField] private GameObject _character;
+
+    [Space(16)]
+    [InfoBox("The camera used by the character")]
+    [SerializeField] private Camera _camera;
+
+    [Space(16)]
+    [ReadOnly]
+    [SerializeField] private int _currentHealth;
+
+    [Space(16)]
+    [AssetOnly]
+    [SerializeField] private Material _playerMaterial;
+
+    [Space(16)]
+    [SceneOnly]
+    [SerializeField] private Rigidbody _rigidbody;
+
+    [Space(16)]
+    [SerializeField] private bool _isCanRun;
+
+    [ShowIf("_isCanRun")]
+    [SerializeField] private int _runSpeed;
+
+    [Button]
+    public void Jump()
+    {
+
+    }
+
+    [Button]
+    public void AddHealth(int health)
+    {
+
+    }
+}
---
+++ Scripts/ShowIfDrawer.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEditor;
+
+[System.AttributeUsage(System.AttributeTargets.Field, Inherited = true, AllowMultiple = false)]
+public class ShowIfAttribute : PropertyAttribute
+{
+    public string conditionName;
+
+    public ShowIfAttribute(string conditionName)
+    {
+        this.conditionName = conditionName;
+    }
+}
+
+#if UNITY_EDITOR
+
+[CustomPropertyDrawer(typeof(ShowIfAttribute))]
+public class ShowIfDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        if (IsPropertyVisible(property))
+        {
+            EditorGUI.PropertyField(position, property, label, true);
+        }
+    }
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        if (!IsPropertyVisible(property))
+        {
+            return 0f;
+        }
+
+        return EditorGUI.GetPropertyHeight(property, label, true);
+    }
+
+    private bool IsPropertyVisible(SerializedProperty property)
+    {
+        ShowIfAttribute showIf = (ShowIfAttribute)attribute;
+
+        string propertyPath = property.propertyPath;
+        string conditionPath = propertyPath.Replace(property.name, showIf.conditionName);
+
+        SerializedProperty conditionProperty = property.serializedObject.FindProperty(conditionPath);
+
+        if (conditionProperty != null && conditionProperty.propertyType == SerializedPropertyType.Boolean)
+        {
+            return conditionProperty.boolValue;
+        }
+
+        return false;
+    }
+}
+
+#endif
