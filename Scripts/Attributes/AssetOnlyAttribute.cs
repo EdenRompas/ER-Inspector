2b93e708b1333ea03ab74c7955ebf77c8a90811d
Eden Rompas <93896553+EdenRompas@users.noreply.github.com>
2026-08-27 20:02:36 +0800

    Delete Scripts/Attributes directory

--- Scripts/Attributes/AssetOnlyAttribute.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-namespace ERInspector
-{
-    [System.AttributeUsage(System.AttributeTargets.Field, Inherited = true, AllowMultiple = false)]
-    public class AssetOnlyAttribute : PropertyAttribute { }
-}
--- Scripts/Attributes/ButtonAttribute.cs
+++
@@ -1,7 +0,0 @@
-using System;
-
-namespace ERInspector
-{
-    [AttributeUsage(AttributeTargets.Method, Inherited = true, AllowMultiple = false)]
-    public class ButtonAttribute : Attribute { }
-}
